8cfb9eeff579bcab0e03c8828fd0526ae0bb4729
s40213 <74708041+s40213@users.noreply.github.com>
2024-09-10 11:16:05 +0800

    Delete kmxt directory

--- kmxt/Get Start.php
+++
@@ -1,9 +0,0 @@
-<?
-$USER=$_POST["USER"];
-$file_path = "Data/".$USER."/".$USER.".start";
-if(file_exists($file_path)){
-$fp = fopen($file_path,"r");
-$str = fread($fp,filesize($file_path));
-fclose($fp);
-echo $str = str_replace("\r\n","<br />",$str);
-}
--- kmxt/Test Id.php
+++
@@ -1,8 +0,0 @@
-<?
-$USER=$_POST["USER"];
-$file_path = "Data/".$USER."/".$USER.".id";
-if(is_file($file_path)){
-echo "存在";
-}else{
-echo "不存在";
-}
